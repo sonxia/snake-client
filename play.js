e0fa5b0ac625e2724485f84e37377f6e839fa7ad
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-27 17:57:08 +0000

    added module exports

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require("net");
+const {connect} = require("./client.js")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('connect', () => {
-    console.log("connected to server!!!");
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+  
