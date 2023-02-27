8033f9d70f47e6d492a1631b317f934ad37ed3cc
Sonia Mudhar <sonia.mudhar@ucalgary.ca>
2023-02-27 08:37:22 +0000

    play.js

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log("connected to server!!!");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
